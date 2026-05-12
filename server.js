0bba6aaf8edccad41cc0f313671a5df5249f603d
ahsanf57rr-hash <ahsan.f57.rr@gmail.com>
2026-05-12 05:27:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.post("/chat", async (req, res) => {
 
     const model =
       genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.0-flash",
       });
 
     const prompt = `
